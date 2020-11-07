c29adac75b94ee3d3275d80752f012cd049961da
antonzaharia <zaharia_anton@yahoo.com>
2020-11-07 15:13:10 +0000

    login action

---
+++ frontend-chat-app/src/actions/UserActions.js
@@ -0,0 +1,16 @@
+export function login (user) {
+    return(dispatch) => {
+        fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(user)
+        }).then((resp) => resp.json())
+        .then((result) => {
+            if("errors" in result) {
+                dispatch({type: "FAILED", payload: result.errors})
+            } else {
+                dispatch({type: "SIGNUP", payload: result})
+            }
+        })
+    }
+}
