92305d3f4c842a76e05f9dac4cfc3dbe6b500c96
antonzaharia <zaharia_anton@yahoo.com>
2020-11-08 17:58:49 +0000

    sessions controller created

---
+++ backend-chat-app/config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookies_store, key: "_authentication_app", domain: "localhost:3000"
+else
+    Rails.application.config.session_store :cookies_store, key: "_authentication_app"
+end
--- backend-chat-app/config/routes.rb
+++ backend-chat-app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'session/create'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
