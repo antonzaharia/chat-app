739c23cb8d003b20ce103b8a61e77898826fca47
antonzaharia <zaharia_anton@yahoo.com>
2020-11-11 23:32:52 +0000

    session_store error fixed

--- backend-chat-app/config/initializers/session_store.rb
+++ backend-chat-app/config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env = 'production'
+if Rails.env == 'production'
     Rails.application.config.session_store :cookies_store, key: "_backend_chat_app", domain: "localhost:3000"
 else
     Rails.application.config.session_store :cookies_store, key: "_backend_chat_app"
