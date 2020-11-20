6c7119a66b389d733fc3b3e3d8491092554abcb1
antonzaharia <zaharia_anton@yahoo.com>
2020-11-20 21:26:36 +0000

    associations created

--- backend-chat-app/app/models/user.rb
+++ backend-chat-app/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+
+    has_many :messages
+    has_many :conversations, through: :messages
     
     validates :name, presence: true
     validates :email, presence: true
--- backend-chat-app/db/migrate/20201120210851_create_conversations.rb
+++ backend-chat-app/db/migrate/20201120210851_create_conversations.rb
@@ -1,7 +1,6 @@
 class CreateConversations < ActiveRecord::Migration[6.0]
   def change
     create_table :conversations do |t|
-      
 
       t.timestamps
     end
