ff0a6364efc1ac4c25cc2642a4d9821034cc0f29
antonzaharia <zaharia_anton@yahoo.com>
2020-11-24 17:03:46 +0000

    handle messages

--- frontend-chat-app/src/App.js
+++ frontend-chat-app/src/App.js
@@ -26,6 +26,37 @@ class App extends Component {
       .then(res => res.json())
       .then(messages => this.setState({ messages: messages });
   }
+
+  createSubscription = () => {
+    this.cable.subscriptions.create(
+      { channel: 'MessagesChannel' },
+      { received: message => this.handleReceivedMessage(message) }
+    )
+  }
+  mapMessages = () => {
+    return this.state.messages.map((message, i) => 
+      <li key={i}>{message.content}</li>)
+  }
+  handleReceivedMessage = message => {
+    this.setState({ messages: [...this.state.messages, message] })
+  }
+  handleMessageSubmit = e => {
+    e.preventDefault();
+    const messageObj = {
+      message: {
+        content: e.target.message.value
+      }
+    }
+    const fetchObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageObj)
+    }
+    fetch('http://localhost:3000/messages', fetchObj)
+    e.target.reset()
+  }
   render() {
     return (
       <Container d="flex" justify="center">
