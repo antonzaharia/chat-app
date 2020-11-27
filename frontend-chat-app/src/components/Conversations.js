a6e54c827fe6035a83c97167c735c5a3fdd777d6
antonzaharia <zaharia_anton@yahoo.com>
2020-11-27 17:49:33 +0000

    conversations link

--- frontend-chat-app/src/components/Conversations.js
+++ frontend-chat-app/src/components/Conversations.js
@@ -1,4 +1,5 @@
 import { Div } from 'atomize'
+import { Link } from 'react-router-dom'
 import React, { Component } from 'react'
 
 class Conversations extends Component {
@@ -7,10 +8,13 @@ class Conversations extends Component {
         conversation.users ? conversation.users.map( user => name = name + "| " + user.name + " |") : name = "Nothing Here"
         return name
     }
+    makeConversationLink = (conversation) => {
+        return `/conversation/${conversation.id}`
+    }
     render() {
         return (
             <Div>
-                {this.props.conversations.map( c => <p key={c.id}>{this.conversationName(c)}</p>)}
+                {this.props.conversations.map( c => <Link to={this.makeConversationLink(c)} key={c.id}>{this.conversationName(c)}</Link>)}
             </Div>
         )
     }
