b3875719c940fd7e90ee698f5d631b645ca8bff5
antonzaharia <zaharia_anton@yahoo.com>
2020-11-29 16:11:36 +0000

    messages create method

--- backend-chat-app/app/controllers/messages_controller.rb
+++ backend-chat-app/app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
 
     def create
         conversation = Conversation.find(message_params[:conversation_id])
-        message = conversation.messages.build(content: message_params[:content], user: @current_user)
+        message = conversation.messages.create(content: message_params[:content], user: @current_user)
         if message.save
             serialized_data = ActiveModelSerializers::Adapter::Json.new(MessageSerializer.new(message)).serializable_hash
               MessagesChannel.broadcast_to conversation, serialized_data
