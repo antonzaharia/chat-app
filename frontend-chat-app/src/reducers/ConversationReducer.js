2688b57dec3a985f4253c32fe3c0c0e8ac8afa98
antonzaharia <zaharia_anton@yahoo.com>
2020-12-09 20:35:21 +0000

    receiving duplicates bug fixed

--- frontend-chat-app/src/reducers/ConversationReducer.js
+++ frontend-chat-app/src/reducers/ConversationReducer.js
@@ -6,10 +6,16 @@ export default function UserReducer( state = { userConversations: []}, action) {
             return {...state, userConversations: [...state.userConversations, action.payload]}
         case "RECEIVE_MESSAGE":
             const findConversation = state.userConversations.find( c => c.id === action.payload.message.conversation.id)
-            findConversation.messages.push(action.payload.message)
-            const filteredConversation = state.userConversations.filter( c => c.id !== action.payload.message.conversation.id)
-            filteredConversation.push(findConversation)
-            return {...state, userConversations: [...filteredConversation]}
+            const messageCheck = findConversation.messages.find( m => m.id === action.payload.message.id)
+            if(messageCheck === undefined){
+              findConversation.messages.push(action.payload.message)
+              const filteredConversation = state.userConversations.filter( c => c.id !== action.payload.message.conversation.id)
+              filteredConversation.push(findConversation)
+              return {...state, userConversations: [...filteredConversation]}
+            } else {
+                return state
+            }
+              
         case "SEND_MESSAGE":
             // const newConversation = state.userConversations.find( c => c.id === action.payload.conversation.id)
             // newConversation.messages.push(action.payload)
